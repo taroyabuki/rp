032e3313affb8c8c836be2fa825a7e10a7993b1f
Taro Yabuki <taroyabuki@users.noreply.github.com>
2021-07-01 22:36:05 +0900

    update

--- code/R/ch03/03.08.R
+++ code/R/ch03/03.08.R
@@ -4,25 +4,6 @@ x <- 123
 typeof(x)
 #> [1] "double"
 
-library(caret)
-library(tidyverse)
-my_data <- cars
-my_model <- train(form = dist ~ speed, data = my_data, method = "knn")
-my_model$results
-# 結果は割愛
-
-attributes(my_model)
-#> $names
-#>  [1] "method"       "modelInfo"    "modelType"    "results"     
-#>  [5] "pred"         "bestTune"     "call"         "dots"        
-#>  [9] "metric"       "control"      "finalModel"   "preProcess"  
-#> [13] "trainingData" "resample"     "resampledCM"  "perfNames"   
-#> [17] "maximize"     "yLimits"      "times"        "levels"      
-#> [21] "terms"        "coefnames"    "xlevels"    
-#> 
-#> $class
-#> [1] "train"         "train.formula"
-
 ?log
 # あるいは
 help(log)
